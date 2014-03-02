d73ebbab06fbe60e429aa6aa7a4b63995dec2935
bibangamba <andrewbibangamba@gmail.com>
2014-03-02 11:56:53 +0300

    corrected ./spec/factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,10 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 
-FactoryGirl.define :user do |user|
-	user.name "bibangamba"
-	user.email "bibangamba@zahard.com"
-	user.password "hazahard"
-	user.password_confirmation "hazahard"
+FactoryGirl.define do
+	factory :user do
+		name 									"yuri"
+		email 								"zahard@TOG.com"
+		password 							"hazahard"
+		password_confirmation "hazahard"
+	end
 end
